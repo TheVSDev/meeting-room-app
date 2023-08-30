cdbb9700ad273f66711e5ad41aa3a9765261829f
TheVSDev <vspasic03@gmail.com>
2023-08-30 20:45:13 +0200

    done: FreeRoom date and time

--- renderer/web/components/FreeRoom.tsx
+++ renderer/web/components/FreeRoom.tsx
@@ -1,12 +1,46 @@
+// Imports
+import React, { useState, useEffect } from "react";
 import styles from "../styles/RoomBook.module.css";
 import RoomBookTitle from "./RoomBookTitle";
 
+// FreeRoom
 const FreeRoom = () => {
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      setCurrentDateTime(new Date());
+    };
+
+    // Update the current date and time every second
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formatDate = (date) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  };
+
+  const formatTime = (date) => {
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    return date.toLocaleTimeString(undefined, options);
+  };
+
   return (
     <div className={styles.body}>
       <div className={styles.containerLeft}>
         <RoomBookTitle titleLabel="FREE" />
-        <button className={styles.btnSchedule}><span className={styles.plus}>+</span></button>
+        <span className={styles.datetime}>
+          {formatTime(currentDateTime)} | {formatDate(currentDateTime)}
+        </span>
+        <button className={styles.btnSchedule}>
+          <span className={styles.plus}>+</span>
+        </button>
       </div>
     </div>
   );
