fd6a56f28314beaecff35b185a77bdfa0ff3d09c
TheVSDev <vspasic03@gmail.com>
2023-09-02 18:22:09 +0200

    done: button to open schedule form

--- renderer/pages/home-page.tsx
+++ renderer/pages/home-page.tsx
@@ -1,13 +1,34 @@
-import HeadPage from "../web/components/HeadPage"
-import FreeRoom from "../web/components/FreeRoom"
+// Imports
+import React, { useState, useEffect } from "react";
 
+import styles from "../web/styles/General.module.css";
+import HeadPage from "../web/components/HeadPage";
+import FreeRoom from "../web/components/FreeRoom";
+import BookRoomForm from "../web/components/BookRoomForm";
+
+// HomePage
 const HomePage = () => {
-    return (
-        <>
-            <HeadPage />
-            <FreeRoom />
-        </>
-    )
-}
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
-export default HomePage
+  // openForm function
+  const openForm = () => {
+    setIsFormOpen(true);
+  };
+
+  // closeForm function
+  const closeForm = () => {
+    setIsFormOpen(false);
+  };
+  return (
+    <>
+      <HeadPage />
+      <FreeRoom />
+      <button className={styles.plusButton} onClick={openForm}>
+        <img src="/images/plus.svg" />
+      </button>
+      {isFormOpen && <BookRoomForm onClose={closeForm} />}
+    </>
+  );
+};
+
+export default HomePage;
--- renderer/web/components/HeadPage.tsx
+++ renderer/web/components/HeadPage.tsx
@@ -5,6 +5,7 @@ const HeadPage = () => {
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>Meeting Room App</title>
+            <link rel="icon" type="image/x-icon" href="/images/meet.svg"></link>
         </Head>
     )
 }
